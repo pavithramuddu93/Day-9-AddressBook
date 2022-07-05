3c4b36fb6ed0985600474f164f2835f553f807fe
pavithramuddu93 <pavithramuddu93>
2022-07-05 20:46:45 +0530

    UC10 added

--- src/com/blz/BookList.java
+++ src/com/blz/BookList.java
@@ -35,6 +35,7 @@ public class BookList {
     }
 
     void showPersonsByCity(String placeName) {
+        int count = 0;
         if (books.size() == 0) {
             System.out.println("Booklist is empty");
             return;
@@ -42,12 +43,15 @@ public class BookList {
         for (int i = 0; i < books.size(); i++) {
             List<Contact> matchedContact = books.get(i).list.stream().filter(x -> x.city.equals(placeName))
                     .collect(Collectors.toList());
+            count += books.get(i).list.stream().filter(x -> x.city.equals(placeName)).count();
             matchedContact.stream().forEach(x -> System.out.println(x.firstName));
 
         }
+        System.out.println("Number of persons are : " + count);
     }
 
     void showPersonsByState(String placeName) {
+        int count = 0;
         if (books.size() == 0) {
             System.out.println("Booklist is empty");
             return;
@@ -55,9 +59,10 @@ public class BookList {
         for (int i = 0; i < books.size(); i++) {
             List<Contact> matchedContact = books.get(i).list.stream().filter(x -> x.state.equals(placeName))
                     .collect(Collectors.toList());
+            count += books.get(i).list.stream().filter(x -> x.state.equals(placeName)).count();
             matchedContact.stream().forEach(x -> System.out.println(x.firstName));
-
         }
+        System.out.println("Number of persons are : " + count);
     }
 
     void operations(ArrayList<AddressBook> books, int i) {
